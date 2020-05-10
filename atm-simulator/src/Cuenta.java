dfa9fbc5d4a8c6bb5f2bdc8f97375f14c2dc7e0a
Matias Giambra <matiasngiambra@gmail.com>
2020-05-10 18:09:52 -0300

    creo la clase "Cuenta"

---
+++ atm-simulator/src/Cuenta.java
@@ -0,0 +1,45 @@
+import SimuladorATM.Cliente;
+
+public abstract class Cuenta {
+
+	private String alias = null;
+	private int cuit;
+	private double saldoPesos = 0;
+	private double saldoDolar=0;
+	
+	public Cuenta (Cliente cliente) {
+		this.cuit = cliente.getCuit();
+			
+	}
+	
+	public void depositar(int monto) {
+		this.setSaldoPesos(this.getSaldoPesos()+monto);
+		
+	}
+	
+	public String getAlias() {
+		return alias;
+	}
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
+	public int getCuit() {
+		return cuit;
+	}
+	public void setCuit(int cuit) {
+		this.cuit = cuit;
+	}
+	public double getSaldoPesos() {
+		return saldoPesos;
+	}
+	public void setSaldoPesos(double saldoPesos) {
+		this.saldoPesos = saldoPesos;
+	}
+	public double getSaldoDolar() {
+		return saldoDolar;
+	}
+	public void setSaldoDolar(double saldoDolar) {
+		this.saldoDolar = saldoDolar;
+	}
+
+}
