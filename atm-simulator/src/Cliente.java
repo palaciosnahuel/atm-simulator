5994d064ef6c9b2e6a227e6f49bb00af70a27ab9
gezek <64788922+gezek@users.noreply.github.com>
2020-05-25 03:03:24 -0300

    Cliente v2 - Se agrega operador switch

--- atm-simulator/src/Cliente.java
+++ atm-simulator/src/Cliente.java
@@ -1,100 +1,205 @@
 
 import java.util.LinkedList;
 
-import account.CajaAhorroDolar;
-import account.CajaAhorroPeso;
 import account.Cliente;
-import account.Cuenta;
 import account.CuentaCorriente;
 import account.Tarjeta;
 
 
 public class Cliente {
 
-		private int cuit;
-		private LinkedList<Cuenta> cuentas;
-		private Tarjeta tarjeta;
-		
-	public Cliente(int cuit, CuentaCorriente cuentaCorriente, String alias, double descubierto, Tarjeta tarjeta) {
-		
-		cuentas = new LinkedList<Cuenta>();
-		this.cuit = cuit;
-		cuentaCorriente.setDescubierto(descubierto);
-		cuentaCorriente.setAlias(alias);
-		cuentas.add(cuentaCorriente);
-		this.tarjeta = tarjeta;
-		
-			
-	}
+	private int cuit;
+	private LinkedList<Cuenta> cuentas;
+	private Tarjeta tarjeta;
 	
-	public Cliente(int cuit, CajaAhorroDolar cuentaDolar, String alias, Tarjeta tarjeta) {
-		
-		cuentas = new LinkedList<Cuenta>();
-		this.cuit = cuit;
-		cuentaDolar.setAlias(alias);
-		cuentas.add(cuentaDolar);
-		this.tarjeta = tarjeta;
-		
-	}
+public Cliente(int cuit, CuentaCorriente cuentaCorriente, String aliasCC, double descubierto, Tarjeta tarjeta) {
 	
-	public Cliente(int cuit, CajaAhorroPeso cuentaPeso, Tarjeta tarjeta) {
-		
-		this.cuit = cuit;
-		cuentas.add(new CajaAhorroPeso(this.cuit));
-		this.tarjeta = tarjeta;
-	}
+	cuentas = new LinkedList<Cuenta>();
 	
-	public Cliente(int cuit, CuentaCorriente cuentaCorriente, double descubierto, CajaAhorroDolar cuentaDolar, CajaAhorroPeso cuentaPeso, Tarjeta tarjeta) {
-		
-		this.cuit = cuit;
-		cuentas.add(new CuentaCorriente(this.cuit, descubierto));
-		cuentas.add(new CajaAhorroDolar(this.cuit));
-		cuentas.add(new CajaAhorroPeso(this.cuit));
-		this.tarjeta = tarjeta;
-		
-	}
+	this.cuit = cuit;
+	this.tarjeta = tarjeta;
 	
-	public Cliente(int cuit, CuentaCorriente cuentaCorriente, double descubierto, CajaAhorroDolar cuentaDolar, Tarjeta tarjeta) {
-		
-		this.cuit = cuit;
-		cuentas.add(new CuentaCorriente(this.cuit, descubierto));
-		cuentas.add(new CajaAhorroDolar(this.cuit));
-		this.tarjeta = tarjeta;
-	}
+	cuentaCorriente.setDescubierto(descubierto);
+	cuentaCorriente.setAlias(aliasCC);
+	cuentas.add(cuentaCorriente);
 	
-	public Cliente(int cuit, CajaAhorroPeso cuentaPeso, CajaAhorroDolar cuentaDolar, Tarjeta tarjeta) {
-		
-		this.cuit = cuit;
-		cuentas.add(new CajaAhorroPeso(this.cuit));
-		cuentas.add(new CajaAhorroDolar(this.cuit));
-		this.tarjeta = tarjeta;
-	}
 	
-	public Cliente(int cuit, CajaAhorroPeso cuentaPeso, double descubierto, CuentaCorriente cuentaCorriente, Tarjeta tarjeta) {
 		
-		this.cuit = cuit;
-		cuentas.add(new CuentaCorriente(this.cuit, descubierto));
-		cuentas.add(new CajaAhorroPeso(this.cuit));
-		this.tarjeta = tarjeta;
-	}
+}
+
+public Cliente(int cuit, CajaAhorroDolar cuentaDolar, String aliasCAD, Tarjeta tarjeta) {
+	
+	cuentas = new LinkedList<Cuenta>();
+	
+	this.cuit = cuit;
+	this.tarjeta = tarjeta;
+	
+	cuentaDolar.setAlias(aliasCAD);
+	cuentas.add(cuentaDolar);
+	
+	
+}
+
+public Cliente(int cuit, CajaAhorroPeso cuentaPeso, String aliasCAP, Tarjeta tarjeta) {
+	
+	cuentas = new LinkedList<Cuenta>();
+	
+	this.cuit = cuit;
+	this.tarjeta = tarjeta;
+	
+	cuentas.add(cuentaPeso);
+	cuentaPeso.setAlias(aliasCAP);
+	
+}
+
+public Cliente(int cuit, CuentaCorriente cuentaCorriente, String aliasCC, double descubierto, CajaAhorroDolar cuentaDolar,String aliasCAD, CajaAhorroPeso cuentaPeso,String aliasCAP, Tarjeta tarjeta) {
+	
+	cuentas = new LinkedList<Cuenta>();
+	
+	this.cuit = cuit;
+	this.tarjeta = tarjeta;
+	
+	cuentas.add(cuentaCorriente);
+	cuentaCorriente.setAlias(aliasCC);
+	cuentaCorriente.setDescubierto(descubierto);
+	
+	cuentas.add(cuentaDolar);
+	cuentaDolar.setAlias(aliasCAD);
+	
+	cuentas.add(cuentaPeso);
+	cuentaPeso.setAlias(aliasCAP);
+	
+	
+}
+
+public Cliente(int cuit, CuentaCorriente cuentaCorriente, String aliasCC, double descubierto, CajaAhorroDolar cuentaDolar, String aliasCAD, Tarjeta tarjeta) {
+	
+	cuentas = new LinkedList<Cuenta>();
+	
+	this.cuit = cuit;
+	this.tarjeta = tarjeta;
+	
+	cuentas.add(cuentaCorriente);
+	cuentaCorriente.setAlias(aliasCC);
+	cuentaCorriente.setDescubierto(descubierto);
+	
+	cuentas.add(cuentaDolar);
+	cuentaDolar.setAlias(aliasCAD);
+	
+}
+
+public Cliente(int cuit, CajaAhorroPeso cuentaPeso, String aliasCAP, CajaAhorroDolar cuentaDolar, String aliasCAD, Tarjeta tarjeta) {
+	
+	cuentas = new LinkedList<Cuenta>();
+	
+	this.cuit = cuit;
+	this.tarjeta = tarjeta;
+	
+	cuentas.add(cuentaDolar);
+	cuentaDolar.setAlias(aliasCAD);
+	
+	cuentas.add(cuentaPeso);
+	cuentaPeso.setAlias(aliasCAP);
+}
+
+public Cliente(int cuit, CajaAhorroPeso cuentaPeso, String aliasCAP, double descubierto, CuentaCorriente cuentaCorriente, String aliasCC, Tarjeta tarjeta) {
+	
+	cuentas = new LinkedList<Cuenta>();
+	
+	this.cuit = cuit;
+	this.tarjeta = tarjeta;
+	
+	cuentas.add(cuentaPeso);
+	cuentaPeso.setAlias(aliasCAP);
+	
+	cuentas.add(cuentaCorriente);
+	cuentaCorriente.setAlias(aliasCC);
+	cuentaCorriente.setDescubierto(descubierto);
+}
 
 	
-	public int getCuit() {
-		return this.cuit;
+public Cliente(int cuit, int tipoDeCuenta, Tarjeta tarjeta, String alias, double descubierto) {
+	
+	switch(tipoDeCuenta) {
+	
+		// Accede a cuenta corriente ( CC )
+	case 01 :
+		
+			
+
+			this.cuit = cuit;
+			this.tarjeta = tarjeta;
+			
+			CuentaCorriente cuentaCorriente = new CuentaCorriente();
+			cuentaCorriente.setAlias(alias);
+			cuentaCorriente.setDescubierto(descubierto);
+			cuentas.add(cuentaCorriente);
+	
+		// Accede a cajaAhorroPeso ( CAP )
+			
+	case 02 :
+
+		this.cuit = cuit;
+		
+		CuentaCorriente cajaAhorroPeso = new CuentaCorriente();
+		cajaAhorroPeso.setAlias(alias);
+		tarjeta = new Tarjeta();
+		cuentas.add(cajaAhorroPeso);
+		
+		
+		// Accede a cajaAhorroDolar ( CAD )
+		
+	case 03 :
+
+		this.cuit = cuit;
+		
+		CuentaCorriente cuentaAhorroDolar = new CuentaCorriente();
+		cuentaAhorroDolar.setAlias(alias);
+		tarjeta = new Tarjeta();
+		cuentas.add(cuentaAhorroDolar);
 	}
+}
+
+public int getCuit() {
+	return this.cuit;
+}
+
+public void agregarCuenta(int tipoDeCuenta, String alias, double descubierto) {
+	
+	switch (tipoDeCuenta) {
+		
+		case 01 :
+			
+			
+			
+			CuentaCorriente cuentaCorriente = new CuentaCorriente();
+			cuentaCorriente.setAlias(alias);
+			cuentaCorriente.setDescubierto(descubierto);
+			cuentas.add(cuentaCorriente);
+			
+		case 02 :
+			
+			
+			
+			CuentaCorriente cajaAhorroPeso = new CuentaCorriente();
+			cajaAhorroPeso.setAlias(alias);
+			cuentas.add(cajaAhorroPeso);
+			
+		case 03 :
+		
+			CuentaCorriente cuentaAhorroDolar = new CuentaCorriente();
+			cuentaAhorroDolar.setAlias(alias);
+			cuentas.add(cuentaAhorroDolar);
+	}
+}
 	public static void main(String[] args) {
 		
-			Cliente juan = new Cliente(36637814,new CuentaCorriente(),"MICHO.TITO.NEGRO",1200, new Tarjeta());
-			System.out.println(juan.tarjeta.getID());
-			System.out.println(juan.cuentas.get(0));
-			System.out.println(juan.cuentas.get(0).getSaldo());
-			System.out.println(juan.cuentas.get(0).getAlias());
-			
-			System.out.println();
-			
-			Cliente rosita = new Cliente(1, new CajaAhorroDolar(),"ROSITA.SOLDIERS.UNTREF", new Tarjeta());
-			System.out.println(rosita.tarjeta.getID());
-			System.out.println(rosita.cuentas.get(0).getSaldo());
-			System.out.println(rosita.cuentas.get(0).getAlias());
+		Cliente juan = new Cliente(36637814,01,new Tarjeta(),"ROSITA.SOLDIERS.UNTREF",1200);
+		juan.agregarCuenta(02, "CAJON.PEPE.MANDARINA", 0);
+		
+		System.out.println(juan.cuentas.get(0).getAlias());
+		System.out.println(juan.tarjeta.getID());
+		System.out.println(juan.cuentas.get(1).getAlias());
+		System.out.println(juan.tarjeta.getID());
 	}
 }
--- atm-simulator/src/Tarjeta.java
+++ atm-simulator/src/Tarjeta.java
@@ -31,36 +31,3 @@ public class Tarjeta {
 		return this.pin;
 	}
 }
-	
-
-	public int getClave() {
-		return clave;
-	}
-	public void setClave(int clave) {
-		this.clave = clave;
-	}
-	public void getNumeroTarjeta() {
-		
-		for(int i=0; i<this.numeroTarjeta.length; ++i) {
-		System.out.println(this.numeroTarjeta[i]);
-		}
-	}
-	public void setNumeroTarjeta() {
-		
-		for(int i=0; i<this.numeroTarjeta.length; ++i) {
-		this.numeroTarjeta[i] = (int)((Math.random()+1)*2);
-		}
-	}
-	public int getCuit() {
-		return cuit;
-	}
-	public void setCuit(int cuit) {
-		this.cuit = cuit;
-	}
-	public Cuenta[] getCuentas() {
-		return cuentas;
-	}
-	public void setCuentas(Cuenta[] cuentas) {
-		this.cuentas = cuentas;
-	}
-}
